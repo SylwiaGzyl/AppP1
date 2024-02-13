8ee63a37e3ef890984d0fedde46d1ddc2819d087
SylwiaGzyl <attano8582@gmail.com>
2024-02-13 16:44:17 +0100

    Main Menu v2
    Main Menu update

--- appp1/src/App.js
+++ appp1/src/App.js
@@ -5,9 +5,7 @@ import { Login } from "./components/Login.jsx";
 import { HomeScreen } from "./components/HomeScreen";
 import { Routes, Route } from 'react-router-dom';
 import { createContext } from "react";
-import { Grafik } from "./components/Grafik";
-import { Urlopy } from "./components/Urlopy";
-import { Zwolnienia } from "./components/Zwolnienia";
+
 
 export const ThemeContext = createContext(null);
 
@@ -25,11 +23,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} /> 
         <Route path="/home" element={< HomeScreen />} />
-        <Route path="/grafik" element={< Grafik />} />
-        <Route path="/urlopy" element={< Urlopy />} />
-        <Route path="/zwolnienia" element={<Zwolnienia />} />
-      </Routes>
-      
+      </Routes>  
       <button className="switch-mode-btn" onClick={toggleTheme} checked={theme === "dark"}>Switch color mode</button>
       
       </div>
--- appp1/src/components/HomeScreen.jsx
+++ appp1/src/components/HomeScreen.jsx
@@ -1,18 +1,68 @@
 import React, { useState } from "react";
-import { Navbar } from "./Navbar";
+import "./Fontello/css/fontello.css";
 
 
 export const HomeScreen = (props) => {
+    
+    
+    let list = document.querySelectorAll('.navigation li');
+    function activeLink(){
+        list.forEach((item) =>
+        item.classList.remove('hovered'));
+        this.classList.add('hovered');
+    }
+    list.forEach((item) =>
+    item.addEventListener('mouseover', activeLink))
+
     return(
+        
         <div className="main-container">
             
-                <Navbar /> 
-            
-         <section>
-            <form>
-                Witaj! Na tej stronie możesz zobaczyć oraz edytować swoje dane osobowe.
-            </form>
-            </section>    
+            <div className="navigation">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <span class="icons"><i className="icon-cog"></i></span>
+                            <span class="title">DayOFF</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="icons"><i className="icon-menu"></i></span>
+                            <span class="title">Dashboard</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="icons"><i className="icon-user"></i></span>
+                            <span class="title">Profile</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="icons"><i className="icon-calendar-check-o"></i></span>
+                            <span class="title">Settings</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="icons"><i className="icon-question"></i></span>
+                            <span class="title">Help</span>
+                        </a>
+                    </li><li>
+                        <a href="#">
+                            <span class="icons"><i className="icon-logout"></i></span>
+                            <span class="title">Sign Out</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className="main-container">
+                <div className="topbar">
+
+                </div>
+            </div>
         </div>
+        
     )
 }
