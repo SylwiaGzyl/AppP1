a213e8933a9ebe7d71e257eb0a82533a3ef455cb
SylwiaGzyl <attano8582@gmail.com>
2024-02-24 00:10:39 +0100

    "Active" Navigation
    Adding a permanent background for the active menu

--- appp1/src/components/HomeScreen.jsx
+++ appp1/src/components/HomeScreen.jsx
@@ -4,62 +4,60 @@ import { FaUser, FaCalendarAlt } from "react-icons/fa";
 import { MdOutlineSupportAgent, MdLogout } from "react-icons/md";
 import { IoIosAirplane } from "react-icons/io";
 import { FaArrowLeft } from "react-icons/fa6";
-import { useNavigate } from "react-router-dom";
-
+import { useNavigate, useLocation } from "react-router-dom";
 
 export const HomeScreen = (props) => {
-
     const navigate = useNavigate();
+    const location = useLocation();
 
-    return(
-        
+    return (
         <div className="mains">
-            
             <div className="navigation">
                 <ul>
                     <li>
                         <a href>
-                            <span class="icons"><IoIosAirplane /></span>
-                            <span class="title">DayOFF</span>
+                            <span className="icons"><IoIosAirplane /></span>
+                            <span className="title">DayOFF</span>
                         </a>
                     </li>
-                    <li>
+                    <li className={location.pathname === '/home' ? 'active' : ''}>
                         <a href onClick={() => navigate('/home')}>
-                            <span class="icons"><FiActivity /></span>
-                            <span class="title">Dashboard</span>
-                            </a>
+                            <span className="icons"><FiActivity /></span>
+                            <span className="title">Dashboard</span>
+                        </a>
                     </li>
-                    <li>
+                    <li className={location.pathname === '/profile' ? 'active' : ''}>
                         <a href onClick={() => navigate('/profile')}>
-                            <span class="icons"><FaUser /></span>
-                            <span class="title">Profile</span>
+                            <span className="icons"><FaUser /></span>
+                            <span className="title">Profile</span>
                         </a>
                     </li>
-                    <li>
+                    <li className={location.pathname === '/settings' ? 'active' : ''}>
                         <a href onClick={() => navigate('/settings')}>
-                            <span class="icons"><FaCalendarAlt /></span>
-                            <span class="title">Settings</span>
+                            <span className="icons"><FaCalendarAlt /></span>
+                            <span className="title">Settings</span>
                         </a>
                     </li>
-                    <li>
+                    <li className={location.pathname === '/help' ? 'active' : ''}>
                         <a href onClick={() => navigate('/help')}>
-                            <span class="icons"><MdOutlineSupportAgent /></span>
-                            <span class="title">Help</span>
+                            <span className="icons"><MdOutlineSupportAgent /></span>
+                            <span className="title">Help</span>
                         </a>
-                    </li><li>
+                    </li>
+                    <li className={location.pathname === '/' ? 'active' : ''}>
                         <a href onClick={() => navigate('/')}>
-                            <span class="icons"><MdLogout /></span>
-                            <span class="title">Sign Out</span>
+                            <span className="icons"><MdLogout /></span>
+                            <span className="title">Sign Out</span>
                         </a>
                     </li>
                 </ul>
                 <div className="user-picture">
                     <div className="welcome">Hello Patrycja!</div>
-                    <img src="https://media-be.chewy.com/wp-content/uploads/2022/09/27095535/cute-dogs-pembroke-welsh-corgi.jpg" onClick={() => navigate('/profile')}></img>
-             </div>
-             <div className="mode">
-             <span class="icons"><FaArrowLeft />Change the mode!</span>
-             </div>
+                    <img src="https://media-be.chewy.com/wp-content/uploads/2022/09/27095535/cute-dogs-pembroke-welsh-corgi.jpg" onClick={() => navigate('/profile')} alt="User Profile"></img>
+                </div>
+                <div className="mode">
+                    <span className="icons"><FaArrowLeft />Change the mode!</span>
+                </div>
             </div>
             <div className="main-profile">
                 <div className="main-section">
@@ -68,5 +66,4 @@ export const HomeScreen = (props) => {
             </div>
         </div>
     )
-    
 }
